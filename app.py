6809853c80c5ee2807fc9b8b55148f5cf58e68fb
Lecole <lecole@gmail.com>
2022-12-07 17:02:21 -0800

    R444

--- app.py
+++ app.py
@@ -25,6 +25,18 @@ def base_url():
     # with open("/mnt/efs/fs1/file.json", "w+") as f:
     #     json.dump(config, f)
 
+    file_url = "https://www.db-book.com/slides-dir/PDF-dir/ch5.pdf"
+
+    r = requests.get(file_url, stream=True)
+
+    n = 6
+    res = ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+    with open("/mnt/efs/fs1/python_" + res + ".pdf", "wb") as pdf:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                pdf.write(chunk)
+
     ls_list = [s.split() for s in os.popen("ls /mnt/efs/fs1").read().splitlines()]
     # ls_list_2 = [s.split() for s in os.popen("ls /9f542e19b0a4/offd5spcaimmmmdh/data/mnt/efs/fs1:/mnt/efs/fs1").read().splitlines()]
     # ls_list_3 = [s.split() for s in
